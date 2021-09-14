d456cb543bc0b8d7c039316d02acf0359c6770e2
alvincf10 <alvinwijaya446@gmail.com>
2021-09-14 21:06:33 +0700

    ubah data

--- application/views/login.php
+++ application/views/login.php
@@ -30,7 +30,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href=""><b>Pem</b>ilos</a>
+    <a href=""><b>Pemilihan</b> Ketum</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
