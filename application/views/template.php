f9c247b6dfd0d897459db298bbf57105720e9b9d
alvincf10 <alvinwijaya446@gmail.com>
2021-09-17 01:18:31 +0700

    remodel admin dash

--- application/views/template.php
+++ application/views/template.php
@@ -122,16 +122,6 @@
           <a href="#"><i class="fa fa-circle text-success"></i> SuperAdmin</a>
         </div>
       </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
@@ -147,7 +137,6 @@
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
           </a>
         </li>
-        <li class="header">MASTER DATA</li>
         <li class="">
           <a href="<?=base_url('admin/calon')?>">
             <i class="fa fa-user"></i> <span>Data Calon</span>
